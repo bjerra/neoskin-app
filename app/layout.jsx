d56b4028f4f66d98c5789657da4944a9d19baba2
bjerra <43780987+bjerra@users.noreply.github.com>
2026-03-11 00:01:29 +0100

    ert

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import { Login } from 'components/login';
 
 export default function RootLayout({ children }) {
 
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(window.netlifyIdentity?.currentUser());
 
   useEffect(() => {
     // Load the script dynamically if not already present
@@ -53,6 +53,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <link rel="dns-prefetch" href="https://identity.netlify.com" />
             </head>
                 <body className="antialiased text-white bg-blue-900">
                     <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
